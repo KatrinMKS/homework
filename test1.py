19eb6d40fecb9b99290b1fb83fde500ac3fa31ff
KatrinMKS <74464735+KatrinMKS@users.noreply.github.com>
2020-12-21 09:33:01 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,9 +5,35 @@ from homework import QueueContainer
 class TestQueueContainer(unittest.TestCase):
 
     def _init_(self):
+        
         self.test_queue = collections.deque()
+        newQueue = QueueContainer
 
-    def test_area(self):
-        self.assertEqual(queue.my_append(10), self.test_queue.append(10))
-        self.assertEqual(queue.my_append(0), self.test_queue.append(0))
-        self.assertEqual(queue.my_append(1210), self.test_queue.append(1210))
+        newQueue.append(10)
+        newQueue.append(0)
+        newQueue.append(1202)
+        self.test_queue.append(10)
+        self.test_queue.append(0)
+        self.test_queue.append(1202)
+
+    def test_append(self):
+        self.assertEqual(newQueue.queue[0], self.test_queue[0])
+        self.assertEqual(newQueue.queue[1], self.test_queue[1])
+        self.assertEqual(newQueue.queue[2], self.test_queue[2])
+
+    def test_showQueue(self):
+        self.asserEqual(newQueue.showQueue(), self.test_queue)
+
+    def test_popleft(self):
+
+        newQueue.popleft()
+        self.test_queue.popleft()
+        self.asserEqual(newQueue.showQueue(), self.test_queue)
+
+        newQueue.popleft()
+        self.test_queue.popleft()
+        self.asserEqual(newQueue.showQueue(), self.test_queue)
+
+        newQueue.popleft()
+        self.test_queue.popleft()
+        self.asserEqual(newQueue.showQueue(), self.test_queue)
