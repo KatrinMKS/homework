e26abdf400cd3ac3ae800491b7fe88aebb958b5c
KatrinMKS <74464735+KatrinMKS@users.noreply.github.com>
2020-12-24 19:59:38 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,32 +1,34 @@
 class Node:
-      def __init__(self, info):
-            self.info = info
-            self.next = None
+    def __init__(self, info):
+        self.info = info
+        self.next = None
+
+            
 class Queue:
-      def __init__(self):
-            self.front = self.rear = None
-      def isEmpty(self):
-            return self.front == None
-      def size(self, size):
-            self.size = size
-      def enQueue(self, element):
-            temp = Node(element)
-            if self.rear == None:
-                  self.front = self.rear = temp
-                  return
-            self.rear.next = temp
-            self.rear = temp
-            def size_num(self, size):
-                  for i in Queue:
-                        size += 1
-                        return size
-      def deQueue(self):
-            if self.isEmpty():
-                  return
-            temp = self.front
-            self.front = temp.next
-            if(self.front == None):
-                  self.rear = None
-            def size_clear(self, size):
-                  size-=1
-                  return size
+    def __init__(self):
+        self.front = self.rear = None
+    def isEmpty(self):
+        return self.front == None
+    def size(self, size):
+        self.size = size
+    def enQueue(self, element):
+        temp = Node(element)
+        if self.rear == None:
+              self.front = self.rear = temp
+              return
+        self.rear.next = temp
+        self.rear = temp
+        def size_num(self, size):
+              for i in Queue:
+                    size += 1
+                    return size
+    def deQueue(self):
+        if self.isEmpty():
+              return
+        temp = self.front
+        self.front = temp.next
+        if(self.front == None):
+              self.rear = None
+        def size_clear(self, size):
+              size-=1
+              return size
