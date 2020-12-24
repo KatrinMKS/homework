ce33375e1a563c3211dfd2fff2b58e15e9e72f49
KatrinMKS <74464735+KatrinMKS@users.noreply.github.com>
2020-12-24 19:55:03 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,31 +1,31 @@
-class Node: 
-      def __init__(self, info): 
-            self.info = info 
+class Node:
+      def __init__(self, info):
+            self.info = info
             self.next = None
-class Queue: 
-      def __init__(self): 
+class Queue:
+      def __init__(self):
             self.front = self.rear = None
-      def isEmpty(self): 
+      def isEmpty(self):
             return self.front == None
       def size(self, size):
             self.size = size
-      def enQueue(self, element): 
-            temp = Node(element) 
-            if self.rear == None: 
-                  self.front = self.rear = temp 
+      def enQueue(self, element):
+            temp = Node(element)
+            if self.rear == None:
+                  self.front = self.rear = temp
                   return
-            self.rear.next = temp 
-            self.rear = temp 
+            self.rear.next = temp
+            self.rear = temp
             def size_num(self, size):
                   for i in Queue:
                         size += 1
                         return size
-      def deQueue(self): 
-            if self.isEmpty(): 
+      def deQueue(self):
+            if self.isEmpty():
                   return
-            temp = self.front 
+            temp = self.front
             self.front = temp.next
-            if(self.front == None): 
+            if(self.front == None):
                   self.rear = None
             def size_clear(self, size):
                   size-=1
