b6b3c7a5f91ba39dcbcfc62de75e8bcf194959ee
KatrinMKS <74464735+KatrinMKS@users.noreply.github.com>
2020-12-24 15:14:52 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -32,15 +32,3 @@ class Queue:
             self.rear = None
   
  
-if __name__== '__main__': 
-    q = Queue() 
-    q.EnQueue(15) 
-    q.EnQueue(23) 
-    q.DeQueue() 
-    q.DeQueue() 
-    q.EnQueue(38) 
-    q.EnQueue(42) 
-    q.EnQueue(56)  
-    q.DeQueue()    
-    print("Queue Front: " + str(q.front.info)) 
-    print("Queue End: " + str(q.rear.info)) 
