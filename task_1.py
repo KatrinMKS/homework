33f2517a5de3790703a273217d54c613acd58d31
KatrinMKS <74464735+KatrinMKS@users.noreply.github.com>
2020-12-24 14:34:19 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,42 +1,46 @@
-class QueueContainer:
-
-    def __init__ (self):
-        self.queue = []
-
-    def append(self, element):
-        self.element = element
-        self.queue.append(self.element)
-
-    def popleft(self):
-        if not self.queue:
-            print ("queue is empty")
-        else:
-            self.queue.reverse()
-            self.queue.pop()
-            self.queue.reverse()
-
-    def showQueue(self):
-        print (self.queue)
-
-queue = QueueContainer()
-queue.append(203)
-queue.showQueue()
-
-queue.append(204)
-queue.showQueue()
-
-queue.append(205)
-queue.showQueue()
-
-queue.append(206)
-queue.showQueue()
-
-queue.popleft()
-queue.showQueue()
-queue.popleft()
-queue.showQueue()
-queue.popleft()
-queue.showQueue()
-queue.popleft()
-queue.showQueue()
-queue.popleft()
+class Starter: 
+      
+    def __init__(self, info): 
+        self.info = info 
+        self.next = None
+  
+class Queue: 
+      
+    def __init__(self): 
+        self.front = self.rear = None
+  
+    def isEmpty(self): 
+        return self.front == None
+      
+    def EnQueue(self, element): 
+        temp = Starter(element) 
+          
+        if self.rear == None: 
+            self.front = self.rear = temp 
+            return
+        self.rear.next = temp 
+        self.rear = temp 
+  
+    def DeQueue(self): 
+          
+        if self.isEmpty(): 
+            return
+        temp = self.front 
+        self.front = temp.next
+  
+        if(self.front == None): 
+            self.rear = None
+  
+ 
+if __name__== '__main__': 
+    q = Queue() 
+    q.EnQueue(15) 
+    q.EnQueue(23) 
+    q.DeQueue() 
+    q.DeQueue() 
+    q.EnQueue(38) 
+    q.EnQueue(42) 
+    q.EnQueue(56)  
+    q.DeQueue()    
+    print("Queue Front: " + str(q.front.info)) 
+    print("Queue End: " + str(q.rear.info)) 
